45eb23c295f3da96f3db546ba79eb5934d2ee058
mbyczyns <mbyczyns@gmail.com>
2024-01-09 22:36:54 +0100

    football

--- Disciplines/FootballMatch.java
+++ Disciplines/FootballMatch.java
@@ -2,17 +2,19 @@ package Disciplines;
 
 import java.util.Random;
 
-public class FootballMatch extends AbstractDiscipline implements DisciplinesInterface{
+public class FootballMatch extends AbstractDiscipline implements DisciplinesInterface {
     @Override
     public int getTotalOdds() {
         return totalOdds;
     }
+
     Random rand = new Random();
     private String teamA;
     private String teamB;
     private int teamAgoals;
     private int teamBgoals;
-    public FootballMatch(String teamA, String teamB){
+
+    public FootballMatch(String teamA, String teamB) {
         this.teamA = "Real Madrid";
         this.teamB = "Barcelona";
         this.teamAgoals = rand.nextInt(6);
@@ -31,7 +33,31 @@ public class FootballMatch extends AbstractDiscipline implements DisciplinesInte
         return teamAgoals;
     }
 
-    public int getTeamBgoals() {
-        return teamBgoals;
+    public boolean getTeamAgoals(int teamAgoals, int teamBgoals) {
+        return (teamBgoals < teamAgoals);
+    }
+
+    public boolean getTeamBgoals(int teamAgoals, int teamBgoals) {
+        return (teamBgoals > teamAgoals);
+    }
+
+    public boolean draw(int teamAgoals, int teamBgoals) {
+        return (teamBgoals == teamAgoals);
+    }
+
+    public boolean more4(int teamAgoals, int teamBgoals) {
+        return (teamBgoals + teamAgoals > 4);
+    }
+
+    public boolean more6(int teamAgoals, int teamBgoals) {
+        return (teamBgoals + teamAgoals > 6);
+    }
+
+    public boolean more8(int teamAgoals, int teamBgoals){
+        return (teamBgoals+teamAgoals>8);
+    }
+
+    public boolean noGoals(int teamAgoals, int teamBgoals){
+        return (teamBgoals==0 || teamAgoals==0);
     }
 }
