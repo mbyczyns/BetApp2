c4070481d0916e73b24d2a60821ee63c9571c013
mbyczyns <mbyczyns@gmail.com>
2024-01-09 16:10:33 +0100

    login progress

--- Main.java
+++ Main.java
@@ -5,14 +5,32 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
+
+    public void createUser(Scanner scanner, List<User> usersList){
+        System.out.println("Creating new user");
+        String givenUsername = scanner.nextLine();
+        boolean correctUsername = false;
+        while (!correctUsername) {
+            System.out.print("Username: ");
+            for (User tempUser : usersList) {
+                if (givenUsername.equals(tempUser.getUsername())) {
+                    System.out.println("User already exists. Try another username.");
+                    correctUsername = false;
+                    break;
+                } else correctUsername = true;
+            }
+
+        }
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        System.out.print("User ID: ");
+        int userID = scanner.nextInt();
+        User user = new User(givenUsername, password, userID);
+        usersList.add(user);
+    }
+
+    public void logIn(String userName, String password, int userID, List<User> usersList,Scanner scanner){
         boolean isLogged = false;
-        List<User> usersList = new ArrayList<>();
-        User klient = new User("marcin","haslomaslo",112);
-        usersList.add(klient);
-        User klient2 = new User("olek", "123123",222);
-        usersList.add(klient2);
         while(!isLogged){
             System.out.print("Your username: ");
             String givenUsername = scanner.nextLine();
@@ -26,5 +44,13 @@ public class Main {
             }
             if(!isLogged) System.out.println("User not found :( Try again.");
         }
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        List<User> usersList = new ArrayList<>();
+        User klient = new User("marcin","haslomaslo",112);
+        usersList.add(klient);
+        User klient2 = new User("olek", "123123",222);
+        usersList.add(klient2);
     }
 }
