676213bc9ccded834e674f464320e007a498d1b0
mbyczyns <mbyczyns@gmail.com>
2024-01-25 00:59:24 +0100

    switch football

--- Coupons/FootballCoupon.java
+++ Coupons/FootballCoupon.java
@@ -15,21 +15,36 @@ public class FootballCoupon  extends AbstractCoupon {
         for (String bet : betList){
             switch (bet){
                 case "awin":
-                    //
+                    if (Bgoals < Agoals) totalOdds*= 3.0F;
+                    else totalOdds *= 0;
+                    break;
                 case "bwin":
-                    //
+                    if (Bgoals > Agoals) totalOdds*= 3.0F;
+                    else totalOdds *= 0;
+                    break;
                 case "draw":
-                    //
+                    if (Bgoals == Agoals) totalOdds*= 8.0F;
+                    else totalOdds *= 0;
+                    break;
                 case "more4":
-                    //
+                    if (Bgoals + Agoals > 4) totalOdds*= 1.5F;
+                    else totalOdds *= 0;
+                    break;
                 case "more6":
-                    //
+                    if (Bgoals + Agoals > 6) totalOdds*= 2.5F;
+                    else totalOdds *= 0;
+                    break;
                 case "more8":
-                    //
+                    if (Bgoals + Agoals > 8) totalOdds*= 4.0F;
+                    else totalOdds *= 0;
+                    break;
                 case "nogoal":
-                    //
+                    if (Bgoals==0 || Agoals==0) totalOdds*= 3.5F;
+                    else totalOdds *= 0;
+                    break;
                 default:
-                    System.out.println("erroreoeoeoero");
+                    System.out.println("A wrong bet has been added. Sorry for your money :(");
+                    totalOdds*=0;
 
 
             }
